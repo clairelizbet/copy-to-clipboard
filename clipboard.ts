6c6cadf7a9f2ea60464c31a87080aa729e276f3a
claire <claire@clairecampbell.io>
2023-08-14 03:08:22 -0700

    Add usage info and cleanup; slap on a CC0 license

--- clipboard.ts
+++ clipboard.ts
@@ -24,14 +24,17 @@ export function copyToClipboard(text: string): Promise<void> {
          * throw an exception. When that happens, the deprecated method of
          * copying to the clipboard will be used as a fallback.
          */
-        .catch(() => copyToClipboardViaSelection(text))
-    );
+        .catch((writeTextError) => {
+          console.error(writeTextError)
+          return copyToClipboardViaSelection(text)
+        })
+    )
 
   /**
    * Use the deprecated method of copying to the clipboard since the modern
    * Clipboard API was not available.
    */
-  return copyToClipboardViaSelection(text);
+  return copyToClipboardViaSelection(text)
 }
 
 /**
@@ -39,14 +42,14 @@ export function copyToClipboard(text: string): Promise<void> {
  * clipboard. This is no longer recommended but is useful as a fallback when
  * the Clipboard API is not available.
  *
- * @deprecated navigator.clipboard is recommended instead when available
+ * @deprecated navigator.clipboard is recommended instead, when available
  */
 async function copyToClipboardViaSelection(text: string): Promise<void> {
   /**
    * Attempt to get a Selection object to be used for selecting the content of
    * a dynamically created text node.
    */
-  const selection = window.getSelection();
+  const selection = window.getSelection()
 
   /**
    * There are some situations where the browser will return a null object.
@@ -54,64 +57,74 @@ async function copyToClipboardViaSelection(text: string): Promise<void> {
    *
    * {@link https://developer.mozilla.org/docs/Web/API/Window/getSelection}
    */
-  if (!selection) throw new Error("Selection object not available");
-
-  /**
-   * This method is non-standard and may not be available on all browsers.
-   *
-   * {@link https://developer.mozilla.org/docs/Web/API/document/execCommand}
-   */
-  if (!document.execCommand)
-    throw new Error("execCommand method not available");
+  if (!selection) throw new Error("Selection object not available")
 
   /**
    * Creates a transient hidden node with the provided text as its content.
    * This node is attached to the end of the document body.
    */
-  const textEl = document.createElement("span");
+  const textEl = document.createElement("span")
 
-  textEl.innerText = text;
-  textEl.style.opacity = "0";
+  textEl.innerText = text
+  textEl.style.opacity = "0"
 
-  document.body.appendChild(textEl);
+  document.body.appendChild(textEl)
 
   /**
    * Resets the current selection, saving its current value for restoration
    * after the copy operation.
    */
-  const priorSelectionRanges: Array<Range> = [];
+  const priorSelectionRanges: Array<Range> = []
 
   for (let rangeIdx = 0; rangeIdx < selection.rangeCount; rangeIdx++) {
-    priorSelectionRanges.push(selection.getRangeAt(rangeIdx));
+    priorSelectionRanges.push(selection.getRangeAt(rangeIdx))
   }
 
-  selection.removeAllRanges();
+  selection.removeAllRanges()
 
   /**
    * Creates a new range object and uses it to select the transient text node.
    */
-  const range = document.createRange();
+  const range = document.createRange()
 
-  range.selectNode(textEl);
-  selection.addRange(range);
+  range.selectNode(textEl)
+  selection.addRange(range)
 
   /**
    * Call the copy command. After the command is executed, remove the transient
    * text node and restore the selected ranges to their value prior to the copy
    * operation regardless of success.
    */
-  const copySucceeded = document.execCommand("copy");
+  let copySucceeded: boolean
+  let copyError: unknown
 
-  document.body.removeChild(textEl);
-  selection.removeAllRanges();
+  /**
+   * This method is non-standard and may not be available on all browsers.
+   *
+   * {@link https://developer.mozilla.org/docs/Web/API/document/execCommand}
+   */
+  try {
+    copySucceeded = document.execCommand("copy")
+  } catch (commandError) {
+    copySucceeded = false
+    copyError = commandError
+  }
+
+  document.body.removeChild(textEl)
+  selection.removeAllRanges()
 
   priorSelectionRanges.forEach((priorSelectionRange) =>
     selection.addRange(priorSelectionRange)
-  );
+  )
+
+  /**
+   * Now that the DOM has been cleaned up, bubble up any Error that was thrown
+   * during the copy operation.
+   */
+  if (copyError) throw copyError
 
   /**
    * If the copy did not succeed, throw an Error.
    */
-  if (!copySucceeded)
-    throw new Error("Copy command is unsupported or disabled");
+  if (!copySucceeded) throw new Error("Copy command is unsupported or blocked")
 }
